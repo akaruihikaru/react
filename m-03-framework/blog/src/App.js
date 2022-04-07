ad014971fb4eaf138825ae66b08ba201ae8ed3a2
Geraldi Theo Wira Kusuma <geralditheo@gmail.com>
2022-04-07 11:53:29 +0700

    React

---
+++ m-03-framework/blog/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+// JSX Javascript XML
+
+function App() {
+  return (
+    // fragment
+    <>
+      {/* XML */}
+      <p className="" >This is Paragraph</p>
+      <button className=" px-8 py-2 rounded outline outline-offset-2 outline-2 outline-pink-500 " >Send</button>
+    </>
+  )
+}
+
+export default App;
